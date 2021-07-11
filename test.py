c1bbb3a930276339188a6820cfea19490044cf8c
5lingxiaomeng <574243996@qq.com>
2021-07-11 21:09:26 +0800

    add cart pushing

--- test.py
+++ test.py
@@ -3,12 +3,19 @@ import rospy
 import time
 from geometry_msgs.msg import Twist
 
+
 class Main:
     def __init__(self):
         rospy.init_node('cart_pushing', anonymous=True)
+        print("start load arm")
+
         self.left_arm = Robot_Api("left_arm")
         self.right_arm = Robot_Api("right_arm")
-        self.cmd_pub = rospy.Publisher('/yocs_cmd_vel_mux/cmd_vel', Twist, queue_size=1)
+        print("arm load succeed")
+
+        self.cmd_pub = rospy.Publisher(
+            '/yocs_cmd_vel_mux/cmd_vel', Twist, queue_size=1)
+
 
     def get_dual_arm_action_state(self):
         left_state = (self.left_arm.client.get_state() == 3)
@@ -19,13 +26,11 @@ class Main:
         self.left_arm.homeRobot()
         self.right_arm.homeRobot()
 
-    def cmd_speed(self,linear_speed, angle_speed):
+    def cmd_speed(self, linear_speed, angle_speed):
         speed = Twist()
         speed.linear.x = linear_speed
         speed.angular.z = angle_speed
         self.cmd_pub.publish(speed)
-
-
 
     def main(self):
         state = 0
@@ -34,23 +39,41 @@ class Main:
         rate = rospy.Rate(100)
         linear_speed = 0
         angle_speed = 0
+        print("start main")
+        n = 0
         while not rospy.is_shutdown():
             if left_end and right_end:
                 if state == 0:
-                    self.left_arm.send_pose((0.47722487688, -0.205575549603, 0.397170269489), (0.640116751194,
-                                                                                          0.33752951026, 0.428907394409, 0.54070597887))
-                    self.right_arm.send_pose((0.47722487688, -0.205575549603, 0.397170269489), (0.640116751194,
-                                                                                           0.33752951026, 0.428907394409, 0.54070597887))                    
+                    left_orientation = [0, 0.7071068, -0.7071068, 0]
+                    right_orientation = [0, 0.7071068, -0.7071068, 0]
+
+                    left_pose = [0.36, -0.64, 0.16]
+                    right_pose = [-0.36, -0.64-0.05, 0.16]
+                    # y-- move forward
+                    self.left_arm.send_pose(left_pose, left_orientation)
+                    self.right_arm.send_pose(right_pose, right_orientation)
+
                     state = 1
                     linear_speed = 0.05
+                    angle_speed = 0.05
                 elif state == 1:
-                    linear_speed = 0   
-            self.cmd_speed(linear_speed,angle_speed)
+                    n+=1
+                    if n>300:
+                        state = 2
+                        linear_speed = 0
+                        angle_speed = 0
+                elif state ==2:
+                    break
+
+                    # break
+            self.cmd_speed(linear_speed, angle_speed)
             left_end, right_end = self.get_dual_arm_action_state()
+            # print left_end
+            # print right_end
             rate.sleep()
 
 
 if __name__ == "__main__":
     main = Main()
-    main.home_dual_arm()
+    # main.home_dual_arm()
     main.main()
